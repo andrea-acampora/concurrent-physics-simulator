c7dbe0776ab9e296b317267da0de5944ff74a073
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-29 14:55:27 +0200

    change master agent

--- src/main/java/pcd01/controller/MasterAgent.java
+++ src/main/java/pcd01/controller/MasterAgent.java
@@ -2,10 +2,12 @@ package pcd01.controller;
 
 import pcd01.model.Body;
 import pcd01.model.SimulationState;
+import pcd01.utils.Chrono;
 import pcd01.view.SimulationView;
 import pcd01.view.View;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MasterAgent extends Thread {
@@ -14,9 +16,9 @@ public class MasterAgent extends Thread {
     private final View view;
     private SimulationState state;
     private int nWorker;
-    TaskBag taskBag;
-    TaskCompletionLatch taskLatch;
-    AbstractTaskFactory taskFactory;
+    private TaskBag taskBag;
+    private TaskCompletionLatch taskLatch;
+    private final AbstractTaskFactory taskFactory;
 
     public MasterAgent(View view, SimulationState state, long maxSteps) {
         this.state = state;
@@ -26,22 +28,49 @@ public class MasterAgent extends Thread {
     }
 
     public void run() {
+
+        log("started");
+
+        Chrono chrono = new Chrono();
+        chrono.start();
+
         this.nWorker = Runtime.getRuntime().availableProcessors() + 1;
         this.taskBag = new TaskBag();
         taskLatch = new TaskCompletionLatch(nWorker);
+
+        log("Create " + nWorker + " worker agents");
         this.createWorkerAgent();
 
         while(state.getSteps() < maxSteps){
+            log("Add computeForcesTasks to the bag");
             this.addComputeForcesTasksToBag();
+
+            log("Waiting completion of first task of this step");
             this.waitStepDone();
+            log("Waking up.. first task finished!");
+
+            log("Add updatePositionTasks to the bag");
             this.addUpdatePositionTasksToBag();
+
+            log("Waiting completion of second task of this step");
             this.waitStepDone();
+            log("Waking up.. second task finished!");
+
+            log("Add checkCollisionTasks to the bag");
             this.addCheckCollisionTasksToBag();
+
+            log("Waiting completion of third task of this step");
             this.waitStepDone();
+            log("Waking up.. third task finished!");
+
+            log("Incrementing step of simulation and display view");
             state.incrementSteps();
             state.setVt(state.getVt() + state.getDt());
             view.display(state);
         }
+
+        chrono.stop();
+        System.out.println("Time elapsed: " + chrono.getTime() / 1000+ " seconds.");
     }
 
     private void waitStepDone() {
@@ -81,7 +110,10 @@ public class MasterAgent extends Thread {
         }
     }
 
+    private void log(String msg){
+        synchronized(System.out){
+            System.out.println("[ master ] " + msg);
+        }
+    }
 
-    //to do elaborations
 }
-
